5b455b58bbedafa4cfb8f21c1be437ebf466a889
OscarEscobar <ing_oscaresc@hotmail.com>
2023-10-05 15:36:43 -0600

    Start the FE Cfe_app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { register, login, logout } from '../services/auth-service.js';
+
+    // Para registrar un nuevo usuario
+    async function handleRegister() {
+        try {
+            const data = await register('Nombre', 'correo@example.com', 'contraseña123');
+            console.log('Datos de registro:', data);
+        } catch (error) {
+            console.error('Error de registro:', error);
+        }
+    }
+
+    // Para iniciar sesión
+    async function handleLogin() {
+        try {
+            const data = await login('correo@example.com', 'contraseña123');
+            console.log('Datos de inicio de sesión:', data);
+        } catch (error) {
+            console.error('Error de inicio de sesión:', error);
+        }
+    }
+
+    // Para cerrar sesión
+    async function handleLogout() {
+        try {
+            const data = await logout();
+            console.log('Desconexión exitosa:', data);
+        } catch (error) {
+            console.error('Error al desconectar:', error);
+        }
+    }
+
+</script>
+
+<button on:click={handleRegister}>Registrar</button>
+<button on:click={handleLogin}>Iniciar Sesión</button>
+<button on:click={handleLogout}>Cerrar Sesión</button>
---
+++ src/services/auth-service.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+
+const API_URL = import.meta.env.VITE_API_BASE_URL;
+console.log('URL de la API:', `${API_URL}/register`);
+export async function register(name, email, password) {
+    try {
+        const response = await axios.post(`${API_URL}/register`, {
+            name,
+            email,
+            password,
+        });
+        console.log('Registro exitoso:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error en el registro:', error.response ? error.response.data : error);
+        throw error;
+    }
+}
+
+export async function login(email, password) {
+    try {
+        const response = await axios.post(`${API_URL}/login`, {
+            email,
+            password,
+        });
+        console.log('Inicio de sesión exitoso:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error en el inicio de sesión:', error.response ? error.response.data : error);
+        throw error;
+    }
+}
+
+export async function logout() {
+    try {
+        const response = await axios.post(`${API_URL}/logout`);
+        console.log('Cierre de sesión exitoso:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error en el cierre de sesión:',  error.response ? error.response.data : error);
+        throw error;
+    }
+}
